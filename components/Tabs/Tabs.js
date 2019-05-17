6c0f2c225d4e57435747802a2cdcf9ddd94fa951
Elise Spain <elisespain@gmail.com>
2019-05-17 08:26:01 -0700

    class TabCard updated

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,7 +23,7 @@ class TabLink {
     // this.cards = Array.from(this.cards).map();
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+     this.tabElement.addEventListener("click", () => this.selectTab());
   }
 
   selectTab(){
@@ -51,11 +51,11 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+     this.cardElement = element;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+     this.cardElement.style.display = "flex";
   }
 
 }
